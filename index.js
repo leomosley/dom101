1b802645727d3f1501475164d729b9cd6fd47c1b
leomosley <leo.mosley2@gmail.com>
2025-01-29 15:45:52 +0000

    completed

--- index.js
+++ index.js
@@ -11,3 +11,168 @@
 'use strict';
 
 // add your functions here
+
+// Test 1
+function replaceText(elem, str) {
+  if (elem instanceof HTMLElement && typeof str === "string") {
+    elem.textContent = str;
+  }
+}
+
+// Test 2
+function addTextTo(elem, str) {
+  if (elem instanceof HTMLElement && typeof str === "string") {
+    elem.textContent += str;
+  }
+}
+
+// Test 3
+function moreBears() {
+  const bears = document.querySelector("#animals");
+
+  if (bears && bears instanceof HTMLImageElement) {
+    bears.src = "http://placebear.com/400/200";
+    bears.alt = "A bear.";
+    bears.title = "A BEAR!";
+  }
+}
+
+// Test 4
+function setId(elem, str) {
+  if (elem instanceof HTMLElement && typeof str === "string") {
+    elem.id = str;
+    return elem;
+  }
+}
+
+// Test 5
+function setClass(elem, str) {
+  if (elem instanceof HTMLElement && typeof str === "string") {
+    elem.className = str;
+    return elem;
+  }
+}
+
+// Test 6
+function addAClass(elem, str) {
+  if (elem instanceof HTMLElement && typeof str === "string") {
+    elem.classList.add(str);
+    return elem;
+  }
+}
+
+// Test 7
+function removeAClass(elem, str) {
+  if (elem instanceof HTMLElement && typeof str === "string") {
+    elem.classList.remove(str);
+    return elem;
+  }
+}
+
+// Test 8
+function newElement(name) {
+  if (typeof name === "string") {
+    const elem = document.createElement(name);
+    return elem;
+  }
+}
+
+// Test 9
+function findElementById(id) {
+  if (typeof id === "string") {
+    const elem = document.querySelector("#" + id);
+    return elem;
+  }
+}
+
+// Test 10
+function findElementsByQuery(query) {
+  const elem = document.querySelectorAll(query);
+  return elem;
+}
+
+// Test 11
+function reverseList(query) {
+  const list = document.querySelector(query);
+
+  if (list instanceof HTMLUListElement || list instanceof HTMLOListElement) {
+    const reversed = [...list.children].reverse();
+    list.innerHTML = "";
+
+    for (const li of reversed) {
+      list.appendChild(li);
+    }
+
+    return list;
+  }
+}
+
+// Test 12
+function mover(moveThis, appendToThis) {
+  const element = document.querySelector(moveThis);
+  const destination = document.querySelector(appendToThis);
+
+  if (element instanceof HTMLElement && destination instanceof HTMLElement) {
+    element.remove();
+    destination.appendChild(element);
+  }
+}
+
+// Test 13
+function filler(list, candidates) {
+  if (list instanceof HTMLElement && candidates instanceof Array) {
+    for (const candidate of candidates) {
+      const li = document.createElement("li");
+      li.textContent = candidate;
+      list.append(li);
+    }
+  }
+}
+
+// Test 14
+function dupe(selector) {
+  const element = document.querySelector(selector);
+
+  if (element && element instanceof HTMLElement) {
+    const parent = element.parentElement;
+    const clone = element.cloneNode(true);
+
+    if (parent) {
+      parent.append(clone);
+    }
+  }
+}
+
+// Test 15
+function removeAll(selector) {
+  const element = document.querySelectorAll(selector);
+
+  if (element) {
+    for (const elem of element) {
+      elem.remove();
+    }
+  }
+}
+
+// Test 16
+function getUserData() {
+  const nameInput = document.querySelector("#username");
+  const speedInput = document.querySelector("#speed");
+  const studentInput = document.querySelector("#student");
+
+  const result = {};
+
+  if (nameInput && nameInput instanceof HTMLInputElement) {
+    result.name = nameInput.value;
+  }
+
+  if (speedInput && speedInput instanceof HTMLInputElement) {
+    result.speed = speedInput.valueAsNumber;
+  }
+
+  if (studentInput && studentInput instanceof HTMLInputElement) {
+    result.student = studentInput.checked;
+  }
+
+  return result;
+}
